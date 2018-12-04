ad214b251e29c260ef261a1d9a153f11aaf3b58b
tyx1703 <tyx1703@gmail.com>
2018-12-04 15:43:39 +0800

    [IM]: specific components of element-ui

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,46 @@
 import Vue from 'vue';
-import Element from 'element-ui';
 
-Vue.use(Element, { size: 'small', zIndex: 3000 });
+import {
+  Container,
+  Header,
+  Aside,
+  Main,
+  Row,
+  Col,
+  Loading,
+  Card,
+  Table,
+  TableColumn,
+  Button,
+  Menu,
+  MenuItem,
+  Submenu,
+  Notification,
+  Message,
+  Form,
+  FormItem,
+  Input,
+  Cascader,
+} from 'element-ui';
+
+Vue.use(Container);
+Vue.use(Header);
+Vue.use(Aside);
+Vue.use(Main);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Loading);
+Vue.use(Card);
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.use(Button);
+Vue.use(Menu);
+Vue.use(MenuItem);
+Vue.use(Submenu);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Input);
+Vue.use(Cascader);
+
+Vue.prototype.$notify = Notification;
+Vue.prototype.$message = Message;
